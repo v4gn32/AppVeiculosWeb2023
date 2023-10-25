ce2df8769a9516a48fa87c12d40ad079ee0e3312
Vagner de Oliveira Florencio <113205762+v4gn32@users.noreply.github.com>
2023-10-25 20:07:28 -0300

    Configuração do Banco de Dados do Projeto

---
+++ AppVeiculosWeb2023/Models/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AppVeiculosWeb2023.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Veiculo> Veiculos { get; set; }
+
+    }
+}
---
+++ AppVeiculosWeb2023/Models/Veiculo.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AppVeiculosWeb2023.Models
+{
+    [Table("Veiculos")]
+    public class Veiculo
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage =("Obrigatório informar o nome!"))]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = ("Obrigatório informar placa!"))]
+        public string Placa { get; set; }
+
+        [Required(ErrorMessage = ("Obrigatório informar o ano de fabricação!"))]
+        public int AnoFabricacao { get; set; }
+
+        [Required(ErrorMessage = ("Obrigatório informar o ano do modelo!"))]
+        public int AnoModelo { get; set; }
+    }
+}
